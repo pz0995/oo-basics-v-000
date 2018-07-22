bf541b006065b72d85cec95c8b7bc65a7b23fec2
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 09:00:48 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end# Make your shoe class here!
